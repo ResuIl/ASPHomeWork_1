4ab5a3bdc8422a7a5b5b071bd9f5af9236acf97c
Rəsul <71267982+ResuIl@users.noreply.github.com>
2023-07-03 00:35:50 +0400

    Add project files.

---
+++ ASPHomeWork_1/Middlewares/LoginCheck.cs
@@ -0,0 +1,18 @@
+﻿namespace ASPHomeWork_1.Middlewares;
+
+public class LoginCheck
+{
+    private RequestDelegate next;
+
+    public LoginCheck(RequestDelegate next)
+    {
+        this.next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        Console.WriteLine("LOGIN CHECK MiddleWare Started");
+        await next.Invoke(context);
+        Console.WriteLine("LOGIN CHECK MiddleWare Ended");
+    }
+}
